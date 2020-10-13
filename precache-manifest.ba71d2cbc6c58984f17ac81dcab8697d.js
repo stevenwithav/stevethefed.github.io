c87a177f77f1c337ef4f411f45e503f201c931cc
nettlesoup <sanettleingham@hotmail.co.uk>
2020-10-13 08:30:28 +0100

    Updates

---
+++ precache-manifest.ba71d2cbc6c58984f17ac81dcab8697d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62dbf3da5acd2ace5579c1452452691f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcc1d4131e65b0509e2b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a58a6b03d4298daa15d",
+    "url": "/static/js/2.34bbb420.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.34bbb420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcc1d4131e65b0509e2b",
+    "url": "/static/js/main.c5f5b620.chunk.js"
+  },
+  {
+    "revision": "874098da9c90c3559b80",
+    "url": "/static/js/runtime-main.7937c41c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
